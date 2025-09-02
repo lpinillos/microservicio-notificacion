328062edc448ee73be136b19bc0208104785aece
Luis Pinillos <pinillosluisfer@gmail.com>
2025-09-01 23:53:58 -0500

    first commit

--- src/main/java/co/analisys/biblioteca/controller/NotificacionController.java
+++ src/main/java/co/analisys/biblioteca/controller/NotificacionController.java
@@ -3,6 +3,7 @@ package co.analisys.biblioteca.controller;
 import co.analisys.biblioteca.dto.NotificacionDTO;
 import co.analisys.biblioteca.service.NotificacionService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,6 +13,7 @@ public class NotificacionController {
     private NotificacionService notificacionService;
 
     @PostMapping
+    @PreAuthorize("hasAnyRole('ROLE_LIBRARIAN', 'ROLE_USER')")
     public void enviarNotificacion(@RequestBody NotificacionDTO notificacion) {
         notificacionService.enviarNotificacion(notificacion);
     }
